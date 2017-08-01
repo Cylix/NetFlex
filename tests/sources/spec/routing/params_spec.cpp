922fc09d4c552e45bac14b2bbbd1728c26533103
Simon Ninon <simon.ninon@whova.com>
2017-08-01 00:23:29 -0700

    params_spec

--- tests/sources/spec/routing/params_spec.cpp
+++ tests/sources/spec/routing/params_spec.cpp
@@ -19,3 +19,9 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+#include <gtest/gtest.h>
+
+#include <netflex/netflex>
+
+//! nothing to test
